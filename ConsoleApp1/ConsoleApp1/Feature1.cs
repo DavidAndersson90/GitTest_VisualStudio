daf0c048ceaf9f8167f335638a6ceaff12d4604a
David Andersson <david.per.andersson@outlook.com>
2020-09-01 08:21:43 +0200

    Added method Add

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -7,5 +7,12 @@ namespace ConsoleApp1
     class Feature1
     {
         //Code
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
